51b9448743c8b81a05d95704cc6ff0635e0354fd
unknown <nishonovamuattar39@gmail.com>
2024-12-08 11:13:52 +0500

    new update

--- movies/context_processors.py
+++ movies/context_processors.py
@@ -1,6 +1,7 @@
 from .models import Movie
 
 def movie_detail_slug(request):
-    movie_detail_slug = Movie.objects.first().slug
+    if Movie.objects.first():
+        movie_detail_slug = Movie.objects.first().slug
 
     return {'movie_detail_slug': movie_detail_slug}
